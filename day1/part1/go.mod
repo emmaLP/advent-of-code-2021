c9b20e4ee91b4df0e4f6d3151a7fc49929e3a099
Emma Roberts <emma.roberts@gymshark.com>
2021-12-01 13:32:01 +0000

    Day 1 part 1

---
+++ day1/part1/go.mod
@@ -0,0 +1,3 @@
+module github.com/emmalp/advent-of-code-2021/day1/part1
+
+go 1.17
---
+++ day1/part1/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"io"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	file, _ := os.Open("input.txt")
+	fileReader := bufio.NewReader(file)
+	ints, err := ReadInts(fileReader)
+	if err != nil {
+		log.Fatal("An error occurred.", err)
+	}
+	log.Print(ints)
+
+	count := countLargerMeasurements(ints)
+	log.Printf("Count of Measurements:, %d", count)
+}
+
+func countLargerMeasurements(values []int) int {
+	var count = 0
+	for i := 0; i < len(values); i++ {
+		if i == 0 {
+			continue
+		}
+		if values[i] > values[i-1] {
+			log.Println(values[i])
+			count++
+		}
+	}
+	return count
+}
+
+func ReadInts(r io.Reader) ([]int, error) {
+	scanner := bufio.NewScanner(r)
+	scanner.Split(bufio.ScanWords)
+	var result []int
+	for scanner.Scan() {
+		x, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			return result, err
+		}
+		result = append(result, x)
+	}
+	return result, scanner.Err()
+}
