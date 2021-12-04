01e6a48dd4b340c5caa5d9f4095b2835f8bdca1f
Emma Roberts <emma.roberts@gymshark.com>
2021-12-04 13:41:09 +0000

    Day 6 - structure

---
+++ day6/part1/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"github.com/emmalp/advent-of-code-2021/pkg/common"
+	"path/filepath"
+)
+
+func main() {
+	absPath, _ := filepath.Abs("day6/input-example.txt")
+	//absPath, _ := filepath.Abs("day6/input.txt")
+	lines := common.ReadLines(absPath)
+	fmt.Println(lines)
+
+}
