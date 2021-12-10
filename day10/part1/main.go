d2122dad6e22f03b38d1f926790a8eac8980db9e
Emma Roberts <emma.roberts@gymshark.com>
2021-12-10 08:17:22 +0000

    Day 10

--- day10/part1/main.go
+++ day10/part1/main.go
@@ -6,10 +6,48 @@ import (
 	"path/filepath"
 )
 
-func main() {
-	absPath, _ := filepath.Abs("day10/input-example.txt")
-	//absPath, _ := filepath.Abs("day10/input.txt")
-	lines := common.ReadLines(absPath)
-	fmt.Println(lines)
+var pairs = map[byte]byte{
+	'(': ')',
+	'{': '}',
+	'[': ']',
+	'<': '>',
+}
 
+var scores = map[byte]int{
+	')': 3,
+	']': 57,
+	'}': 1197,
+	'>': 25137,
+}
+
+func main() {
+	//absPath, _ := filepath.Abs("day10/input-example.txt")
+	absPath, _ := filepath.Abs("day10/input.txt")
+	lines := common.ReadLines(absPath)
+
+	res := 0
+	for _, line := range lines {
+
+		if wrongChar := findIllegalCharacter(line); wrongChar != 0 {
+			res += scores[wrongChar]
+		}
+	}
+	fmt.Println("Answer: ", res)
+}
+
+func findIllegalCharacter(str string) byte {
+	var legalChars []byte
+	var wrongChar byte
+	for _, c := range []byte(str) {
+		c2, ok := pairs[c]
+		if ok {
+			legalChars = append(legalChars, c2)
+		} else if len(legalChars) > 0 && legalChars[len(legalChars)-1] == c {
+			legalChars = legalChars[:len(legalChars)-1]
+		} else {
+			wrongChar = c
+			break
+		}
+	}
+	return wrongChar
 }
---
+++ day10/part2/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"github.com/emmalp/advent-of-code-2021/pkg/common"
+	"path/filepath"
+	"sort"
+)
+
+var pairs = map[byte]byte{
+	'(': ')',
+	'{': '}',
+	'[': ']',
+	'<': '>',
+}
+
+var scores = map[byte]int{
+	')': 1,
+	']': 2,
+	'}': 3,
+	'>': 4,
+}
+
+func main() {
+	//absPath, _ := filepath.Abs("day10/input-example.txt")
+	absPath, _ := filepath.Abs("day10/input.txt")
+	lines := common.ReadLines(absPath)
+
+	allscores := []int{}
+	for _, line := range lines {
+
+		score, wrongChar := determineClosingSequenceScores(line)
+		if wrongChar != 0 {
+			continue
+		}
+		allscores = append(allscores, score)
+	}
+	sort.Ints(allscores)
+	fmt.Println(allscores[len(allscores)/2])
+}
+
+func determineClosingSequenceScores(str string) (int, byte) {
+	var legalChars []byte
+	var wrongChar byte
+	for _, c := range []byte(str) {
+		c2, ok := pairs[c]
+		if ok {
+			legalChars = append(legalChars, c2)
+		} else if len(legalChars) > 0 && legalChars[len(legalChars)-1] == c {
+			legalChars = legalChars[:len(legalChars)-1]
+		} else {
+			wrongChar = c
+			break
+		}
+	}
+
+	if wrongChar != 0 {
+		return 0, wrongChar
+	}
+	score := 0
+	for i := len(legalChars) - 1; i >= 0; i-- {
+		score = score*5 + scores[legalChars[i]]
+	}
+	return score, wrongChar
+}
