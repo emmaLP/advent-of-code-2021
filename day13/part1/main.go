4a9f25b762e441145b75713a5ac43b506eecaf4a
Emma Roberts <emma.roberts@gymshark.com>
2021-12-13 06:34:31 +0000

    Day 13

--- day13/part1/main.go
+++ day13/part1/main.go
@@ -4,10 +4,53 @@ import (
 	"fmt"
 	"github.com/emmalp/advent-of-code-2021/pkg/common"
 	"path/filepath"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	absPath, _ := filepath.Abs("day13/input-example.txt")
-	// absPath, _ := filepath.Abs("day13/input.txt")
+	//absPath, _ := filepath.Abs("day13/input-example.txt")
+	absPath, _ := filepath.Abs("day13/input.txt")
 	lines := common.ReadLines(absPath)
+	coords, instructions := populateCoordsAndInstructions(lines)
+
+	seen := map[[2]int]bool{}
+	for _, p := range coords {
+		firstInstruction := instructions[0][1]
+		if p[0] > firstInstruction {
+			p[0] = 2*firstInstruction - p[0]
+		}
+		seen[p] = true
+	}
+	fmt.Println("Answer:", len(seen))
+}
+
+func populateCoordsAndInstructions(lines []string) (coords, instructions [][2]int) {
+	populateInstructions := false
+	for _, line := range lines {
+		if strings.TrimSpace(line) == "" {
+			populateInstructions = true
+			continue
+		}
+
+		if !populateInstructions {
+			ps := strings.Split(line, ",")
+			var p [2]int
+			p[0], _ = strconv.Atoi(ps[0])
+			p[1], _ = strconv.Atoi(ps[1])
+			coords = append(coords, p)
+		} else {
+			line = line[len("fold along "):]
+			instructionSplit := strings.Split(line, "=")
+
+			axis := 0
+			if instructionSplit[0] == "y" {
+				axis = 1
+			}
+
+			axisIndex, _ := strconv.Atoi(instructionSplit[1])
+			instructions = append(instructions, [2]int{axis, axisIndex})
+		}
+	}
+	return coords, instructions
 }
---
+++ day13/part2/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+	"github.com/emmalp/advent-of-code-2021/pkg/common"
+	"path/filepath"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	//absPath, _ := filepath.Abs("day13/input-example.txt")
+	absPath, _ := filepath.Abs("day13/input.txt")
+	lines := common.ReadLines(absPath)
+	coords, instructions := populateCoordsAndInstructions(lines)
+
+	for _, instruction := range instructions {
+		for i, coord := range coords {
+			if instruction[0] == 0 {
+				if coord[0] > instruction[1] {
+					coord[0] = 2*instruction[1] - coord[0]
+				}
+			} else {
+				if coord[1] > instruction[1] {
+					coord[1] = 2*instruction[1] - coord[1]
+				}
+			}
+			coords[i] = coord
+		}
+	}
+
+	maxX := 0
+	maxY := 0
+	seen := map[[2]int]bool{}
+
+	for _, coord := range coords {
+		seen[coord] = true
+		if coord[0] > maxX {
+			maxX = coord[0]
+		}
+		if coord[1] > maxY {
+			maxY = coord[1]
+		}
+	}
+	printCode(maxX, maxY, seen)
+
+}
+
+func printCode(maxX, maxY int, populatedValues map[[2]int]bool) {
+	for y := 0; y <= maxY; y++ {
+		for x := 0; x <= maxX; x++ {
+			if populatedValues[[2]int{x, y}] {
+				fmt.Print("#")
+			} else {
+				fmt.Print(" ")
+			}
+		}
+		fmt.Println()
+	}
+}
+
+func populateCoordsAndInstructions(lines []string) (coords, instructions [][2]int) {
+	populateInstructions := false
+	for _, line := range lines {
+		if strings.TrimSpace(line) == "" {
+			populateInstructions = true
+			continue
+		}
+
+		if !populateInstructions {
+			ps := strings.Split(line, ",")
+			var p [2]int
+			p[0], _ = strconv.Atoi(ps[0])
+			p[1], _ = strconv.Atoi(ps[1])
+			coords = append(coords, p)
+		} else {
+			line = line[len("fold along "):]
+			instructionSplit := strings.Split(line, "=")
+
+			axis := 0
+			if instructionSplit[0] == "y" {
+				axis = 1
+			}
+
+			axisIndex, _ := strconv.Atoi(instructionSplit[1])
+			instructions = append(instructions, [2]int{axis, axisIndex})
+		}
+	}
+	return coords, instructions
+}
